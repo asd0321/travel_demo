b3c5a4601ddd49ded7592888577eb65040ac7bd7
asd321 <3190794097@qq.com>
2020-03-10 15:16:02 +0800

    A travel project

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,61 @@
+<template>
+	<div>
+		<div class="title">热销推荐</div>
+		<ul>
+			<router-link tag="li" class="item border-bottom" v-for="item of list" :key="item.id" :to="'/detail/'+item.id">
+				<img class="item-img" :src="item.imgUrl" alt="">
+				<div class="item-info">
+					<p class="item-title">{{item.address}}</p>
+					<p class="item-desc">{{item.name}}</p>
+					<button class="item-button">查看详情</button>
+				</div>
+			</router-link>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default{
+		name:'HomeRecommend',
+		props:{
+			list:Array
+		}
+	}
+</script>
+
+<style lang="stylus" scoped>
+	@import '~styles/mixins.styl'
+	.title
+		margin-top:.2rem
+		line-height:.8rem
+		background:#eee
+		text-indent:.2rem
+	.item
+		overflow:hidden
+		display:flex
+		height:1.7rem
+		.item-img
+			width:1.5rem
+			height:1.5rem
+			padding:.1rem
+		.item-info
+			flex:1
+			padding:.1rem	
+			min-width:0		
+			.item-title
+				line-height:.54rem
+				font-size:.25rem
+				ellipsis()
+			.item-desc
+				line-height:.4rem
+				color:#ccc
+				font-size:.22rem
+				ellipsis()
+			.item-button
+				line-height:.44rem
+				margin-top:.05rem
+				background:#ff9300
+				padding:0 .1rem
+				border-radius:.06rem
+				color:#fff
+</style>
